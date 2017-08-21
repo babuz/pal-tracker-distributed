987382a7366ff13cb4e92b33d4337e49db94336f
babuz <ganeshbabu.rajan@gmail.com>
2017-08-21 15:33:51 +0200

    circuit breaker enabled

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
@@ -1,8 +1,10 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import io.pivotal.pal.tracker.timesheets.ProjectClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +12,7 @@ import org.springframework.web.client.RestOperations;
 
 import java.util.TimeZone;
 
-
+@EnableCircuitBreaker
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
 @EnableEurekaClient
